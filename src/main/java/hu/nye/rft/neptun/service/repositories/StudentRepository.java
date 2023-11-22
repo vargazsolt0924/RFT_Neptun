387f5cacf4e055de14143b5682521da5cf5907da
GoncziTibor <79456470+GoncziTibor@users.noreply.github.com>
2023-11-22 21:34:56 +0100

    sql codes

--- src/main/java/hu/nye/rft/neptun/service/repositories/StudentRepository.java
+++ src/main/java/hu/nye/rft/neptun/service/repositories/StudentRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-        @Query(value = "SELECT HALLGATO_ID FROM HALLGATO WHERE USERNAME = ?1", nativeQuery = true)
+        @Query(value = "SELECT studentId FROM student WHERE userName = ?1", nativeQuery = true)
         Long findIdByUserName(String userName);
 
         boolean existsStudentByUserName(String userName);
--- src/main/java/hu/nye/rft/neptun/service/repositories/TeacherRepository.java
+++ src/main/java/hu/nye/rft/neptun/service/repositories/TeacherRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-    @Query(value = "SELECT TANAR_ID FROM TANAR WHERE USERNAME = ?1", nativeQuery = true)
+    @Query(value = "SELECT teacherId FROM teacher WHERE userName = ?1", nativeQuery = true)
     Long findIdByUserName(String userName);
 
     boolean existsTeacherByUserName(String userName);
