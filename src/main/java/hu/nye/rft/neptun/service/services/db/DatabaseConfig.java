3b04ab9423449a1af71531416a4019aaac6d01d8
vargazsolt0924 <vargazsolt1348@gmail.com>
2023-11-22 20:15:33 +0100

    Make the db and db connection

---
+++ src/main/java/hu/nye/rft/neptun/service/services/db/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package hu.nye.rft.neptun.service.services.db;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .build();
+    }
+
+}
