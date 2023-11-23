b036105a4a56209694643fb9af9af1a263e5473f
vargazsolt0924 <vargazsolt1348@gmail.com>
2023-11-23 19:20:49 +0100

    extend yml

--- src/main/java/hu/nye/rft/neptun/service/exceptions/UserNameAlreadyInUseException.java
+++ src/main/java/hu/nye/rft/neptun/service/exceptions/UserNameAlreadyInUseException.java
@@ -1,5 +1,12 @@
 package hu.nye.rft.neptun.service.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Custom exception to represent when user already exists.
+ */
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Username already in use.")
 public class UserNameAlreadyInUseException extends Exception {
     public UserNameAlreadyInUseException(String message) {
         super(message);
--- src/main/java/hu/nye/rft/neptun/service/exceptions/UserNotFoundException.java
+++ src/main/java/hu/nye/rft/neptun/service/exceptions/UserNotFoundException.java
@@ -1,5 +1,10 @@
 package hu.nye.rft.neptun.service.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UserNotFoundException extends Exception{
     public UserNotFoundException(String message) {
         super(message);
