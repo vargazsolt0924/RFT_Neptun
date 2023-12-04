464380273817ce74163e2e745958499ac6956423
MRPGMR <oroszp26@gmail.com>
2023-12-04 18:49:05 +0100

    adatbázisséma frissítése

--- src/main/java/hu/nye/rft/neptun/service/db/schema.sql
+++ src/main/java/hu/nye/rft/neptun/service/db/schema.sql
@@ -1,37 +1,37 @@
 CREATE TABLE IF NOT EXISTS student (
-    studentId INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    userName VARCHAR(255) NOT NULL
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(255) NOT NULL,
+    student_username VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS subject (
-    subjectId INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    dayOfTheWeek VARCHAR(255) NOT NULL,
-    startTime VARCHAR(255) NOT NULL,
-    durationInMinutes INT NOT NULL,
-    maxStudent INT NOT NULL,
-    kredit INT NOT NULL
+    subject_id INT PRIMARY KEY,
+    subject_name VARCHAR(255) NOT NULL,
+    subject_day_of_the_week VARCHAR(255) NOT NULL,
+    subject_start_time VARCHAR(255) NOT NULL,
+    subject_duration_in_minutes INT NOT NULL,
+    subject_max_students INT NOT NULL,
+    subject_credit INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS teacher (
-    teacherId INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    userName VARCHAR(255) NOT NULL
+    teacher_id INT PRIMARY KEY,
+    teacher_name VARCHAR(255) NOT NULL,
+    teacher_username VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS student_subject (
-    studentId INT,
-    subjectId INT,
-    PRIMARY KEY (studentId, subjectId),
-    FOREIGN KEY (studentId) REFERENCES student(studentId),
-    FOREIGN KEY (subjectId) REFERENCES subject(subjectId)
+    student_student INT,
+    subject_id INT,
+    PRIMARY KEY (student_id, subject_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id),
+    FOREIGN KEY (subject_id) REFERENCES subject(subject_id)
 );
 
 CREATE TABLE IF NOT EXISTS subject_teacher (
-    subjectId INT,
-    teacherId INT,
-    PRIMARY KEY (subjectId, teacherId),
-    FOREIGN KEY (subjectId) REFERENCES subject(subjectId),
-    FOREIGN KEY (teacherId) REFERENCES teacher(teacherId)
+    subject_id INT,
+    teacher_id INT,
+    PRIMARY KEY (subject_id, teacher_id),
+    FOREIGN KEY (subject_id) REFERENCES subject(subject_id),
+    FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
 );
